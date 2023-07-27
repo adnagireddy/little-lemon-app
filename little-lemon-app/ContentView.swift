235361493d9ecdac1a251e406258ea75d53735a2
Ananya Nagireddy <ananya.d.nagireddy@gmail.com>
2023-07-27 11:07:15 -0400

    updated blue globe to yellow sparkles on ContentView

--- little-lemon-app/ContentView.swift
+++ little-lemon-app/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "sparkles")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
+                .foregroundColor(.yellow)
             Text("Hello, world!")
         }
         .padding()
