0c227cbbba4ecca1165b98d435ee8f21d2f34514
Ananya Nagireddy <ananya.d.nagireddy@gmail.com>
2023-07-27 10:33:48 -0400

    Initial Commit

---
+++ little-lemon-app/little_lemon_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  little_lemon_appApp.swift
+//  little-lemon-app
+//
+//  Created by Ananya Nagireddy on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct little_lemon_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
